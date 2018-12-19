589c60d70bcd1f185f868c6f6d9ec772d97a75d5
Alex Mc'key <mckey2002@yandex.ru>
2018-12-19 23:13:55 +0400

    Solution 15 - Not complite - only first step - Complit this solution in Scala Project

---
+++ 2018_CSharp/15/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _15
+{
+    using static BoardObjects;
+
+    enum BoardObjects { Empty, Wall, Elves, Goblin, Unknown }
+    enum Directions { Up, Right, Down, Left }
+
+    struct Pos
+    {
+        public int x;
+        public int y;
+    }
+
+    class Board
+    {
+        BoardObjects[][] board;
+
+        public Board(IEnumerable<string> input)
+        {
+            BoardObjects ParseObjects(char ch)
+            {
+                switch (ch)
+                {
+                    case '#': return Wall;
+                    case '.': return Empty;
+                    case 'G': return Goblin;
+                    case 'E': return Elves;
+                    default: return Unknown;
+
+                }
+            }
+
+            board = input.Select(str => str.Select(ParseObjects).ToArray()).ToArray();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
