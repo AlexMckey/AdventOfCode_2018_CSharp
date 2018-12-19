5f4359c0381542c04e46d1420f82f5d4a3b35553
Alex Mc'key <mckey2002@yandex.ru>
2018-12-19 21:52:26 +0400

    Solution 01 - Complite

---
+++ 2018_CSharp/01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            var freqSeq = input
+                        .Split("\r\n")
+                        .Select(int.Parse);
+            var res1 = freqSeq.Sum();
+
+            var curFreq = 0;
+            var freqs = new HashSet<int>();
+            var isFind = false;
+            do
+                foreach(var f in freqSeq)
+                {
+                    curFreq += f;
+                    if (freqs.Contains(curFreq))
+                    {
+                        isFind = true;
+                        break;
+                    }
+                    freqs.Add(curFreq);
+                }
+            while (!isFind);
+            var res2 = curFreq;
+
+            Console.WriteLine($"{res1}"); // 505
+            Console.WriteLine($"{res2}"); // 72330
+        }
+    }
+}
